f86eec52f0b84b49892e1ffc06d1256ac366659f
samschool789@gmail.com <samschool789@gmail.com>
2022-06-01 21:28:24 -0500

    adder voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,13 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.use(require('./candidateRoutes'));
-router.use(require('./partyRoutes'));
-
-
-
-
-
+router.use(require("./candidateRoutes"));
+router.use(require("./partyRoutes"));
+router.use(require("./voterRoutes"));
 
 module.exports = router;
