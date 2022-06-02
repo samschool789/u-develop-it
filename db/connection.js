3cde6dd6d83787396b53433c535bf3296e00648b
samschool789@gmail.com <samschool789@gmail.com>
2022-06-01 20:45:59 -0500

    api routes

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+// Connect to database
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password 
+  password: 'Whatup789',
+  database: 'election'
+});
+
+module.exports = db;
