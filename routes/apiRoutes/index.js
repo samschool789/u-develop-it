42ee5d9108bf7ff742e5b2af107959449e33e750
samschool789@gmail.com <samschool789@gmail.com>
2022-06-01 22:09:37 -0500

    final lesson code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-
+router.use(require('./voteRoutes'));
 router.use(require("./candidateRoutes"));
 router.use(require("./partyRoutes"));
 router.use(require("./voterRoutes"));
